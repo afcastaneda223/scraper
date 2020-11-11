1a3d69111151613c18b25d398072868ff4dec0c6
afcastaneda223 <af.castaneda223@gmail.com>
2020-11-11 00:52:02 -0300

    rubocop fix

--- scrapper.rb
+++ scrapper.rb
@@ -2,8 +2,8 @@ require 'nokogiri'
 require 'open-uri'
 
 def scrapper
-  url = 'https://relocate.me/search'
-  clear_page = Nokogiri::HTML(open(url))
+  url = open('https://relocate.me/search')
+  clear_page = Nokogiri::HTML(url)
   jobs_array = []
   job_cards = clear_page.css('div.jobs-list__job')
   page_number = 1
@@ -11,8 +11,8 @@ def scrapper
   total = clear_page.css('h1.search-page__title').text.split(' ')[2].to_i.to_f
   last_page = (total / jobs_x_page).round
   while page_number <= last_page
-    numbered_url = "https://relocate.me/search?page=#{page_number}"
-    numbered_clear_page = Nokogiri::HTML(open(numbered_url))
+    numbered_url = open("https://relocate.me/search?page=#{page_number}")
+    numbered_clear_page = Nokogiri::HTML(numbered_url)
     numbered_job_cards = numbered_clear_page.css('div.jobs-list__job')
     numbered_job_cards.each do |x|
       job = {
@@ -27,8 +27,8 @@ def scrapper
     puts " #{porc.to_i.round}%"
   end
   jobs_array.each_with_index do |x, y|
-    puts "#{y+1} - #{x}"
-    puts " "
+    puts "#{y + 1} - #{x}"
+    puts ' '
   end
   puts "There are #{jobs_array.count} jobs to aply"
 end
