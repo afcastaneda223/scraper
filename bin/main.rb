486da25a070a734baee4ac39e5ec701fb6764d74
afcastaneda223 <af.castaneda223@gmail.com>
2020-11-16 20:24:33 -0300

    gramamr gix

--- bin/main.rb
+++ bin/main.rb
@@ -8,10 +8,10 @@ class User
   end
 
   @my_logic = Logic.new
-  puts "there are #{@my_logic.display_jobs_x_page.to_i} jobs per page in #{@my_logic.display_last_page.to_i} pages"
-  puts 'how many pages do you want you scrap?'
+  puts "There are #{@my_logic.display_jobs_x_page.to_i + 1} jobs per page in #{@my_logic.display_last_page.to_i} pages"
+  puts 'How many pages do you want you scrap?'
   @my_logic.valid_number
-  puts 'Do you want to Print (p) or save (s)'
+  puts 'Do you want to print (p) or save (s)?'
   @my_logic.valid_p_s
 end
 
