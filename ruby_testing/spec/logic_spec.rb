0e8ac09456803aef8149c67240beb63e6e804c26
afcastaneda223 <af.castaneda223@gmail.com>
2020-11-17 16:53:23 -0300

    added test for logic

--- ruby_testing/spec/logic_spec.rb
+++ ruby_testing/spec/logic_spec.rb
@@ -4,11 +4,25 @@ describe Logic do
   let(:logic) { Logic.new }
   describe '#valid_number' do
     let(:rand_num) { rand(1..30) }
-    it 'valid input' do
-      allow(logic.validate).to receive(1).and_return(logic.scraper(1))
-    end
-    it 'invalid input' do
-      allow(logic.validate).to receive('asdsa').and_return(logic.scraper(false))
-    end
+    it 'valid number?' do
+        expect(logic.number?(rand_num)).to be true
+      end
+  
+      it 'invalid number_number?' do
+        expect(logic.number?(50)).to be false
+          end
+          it 'invalid string number?' do
+            expect(logic.number?('asdsa')).to be false
+              end
+  end
+  let(:logic) { Logic.new }
+  describe '#valid_p_s' do
+      it 'invalid valid?' do
+        expect(logic.validate_p_s('asdsa')).to be false
+      end
+      it 'invalid valid?' do
+        expect(logic.validate_p_s(1)).to be false
+      end
+
   end
 end
